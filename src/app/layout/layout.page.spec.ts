207019550a5a0c7b0898c5783c54fd0ffb6a671c
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-12-16 10:23:03 +0100

    fixed mistake in layout page spec

--- src/app/layout/layout.page.spec.ts
+++ src/app/layout/layout.page.spec.ts
@@ -23,28 +23,3 @@ describe('LayoutPage', () => {
   });
 });
 
-import { Component } from "@angular/core";
-
-// Custom type that represent a tab data.
-declare type PageTab = {
-  title: string; // The title of the tab in the tab bar
-  icon: string; // The icon of the tab in the tab bar
-  path: string; // The route's path of the tab to display
-};
-
-@Component({
-  selector: "app-layout",
-  templateUrl: "layout.page.html",
-  styleUrls: ["layout.page.scss"],
-})
-export class LayoutPage {
-  tabs: PageTab[];
-
-  constructor() {
-    this.tabs = [
-      { title: "New Trip", icon: "add", path: "create-trip" },
-      { title: "Places Map", icon: "map", path: "places-map" },
-      { title: "Trip List", icon: "list", path: "trip-list" },
-    ];
-  }
-}
--- src/app/layout/layout.page.ts
+++ src/app/layout/layout.page.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 // Custom type that represent a tab data.
-declare type PageTab = {
+type PageTab = {
   title: string; // The title of the tab in the tab bar
   icon: string; // The icon of the tab in the tab bar
   path: string; // The route's path of the tab to display
